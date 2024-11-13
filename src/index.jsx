2d04d47280a1ec51a2e008763b88b7e90f70ae24
Trung Nam <trungnam.work@gmail.com>
2024-11-13 21:46:40 +0700

    fix router

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css'
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Router';
+import { LocationProvider } from './Context/LocationContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <LocationProvider>
+    <RouterProvider router={router} />
+  </LocationProvider>
   </React.StrictMode>
 );
 
