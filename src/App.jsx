3da73af54865ebb53f0fdcc2fa8159fa76ec4eed
Trung Nam <trungnam.work@gmail.com>
2024-11-14 21:09:51 +0700

    Responsive module header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css';
+import './App.scss';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
